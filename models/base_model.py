1645a49a877d582fd21de69fc0c235355891b074
nnejiobioma <nnejiobioma@gmail.com>
2023-05-11 19:28:23 +0000

    created folders for rpject

---
+++ models/base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+This is a base class for other models. It provides funtionality that can be used by other models
+In this instance BaseModel class id defined the will be called from the init function and other functions
+"""
+from uuid import uuid4
+from datetime import datetime
+
+
