6572e4cadc1e3b2f355bd4b1309323720899d8a7
nnejiobioma <nnejiobioma@gmail.com>
2023-05-12 15:04:14 +0000

    Added files

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+"""This inherites Amenities from BaseModel class"""
+
+class Amenity(BaseModel):
+	"""Amenities class"""
+	
+	name = ""
