7f2a2134cd95ffe465e07ec5db79e0bde91f56cd
nnejiobioma <nnejiobioma@gmail.com>
2023-05-12 13:22:51 +0000

    base_model completed

--- models/base_model.py
+++ models/base_model.py
@@ -1,43 +1,49 @@
 #!/usr/bin/python3
 
 """
-This is a base class for other models. It provides funtionality that can be used by other models
+This is a base class for other models.
+It provides funtionality that can be used by other models
 In this instance BaseModel class id defined the will be called from the init function and other functions
 """
 from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-	""" Base Class created with uuid4. Each value containing a unique id for each base models"""
-	import uuid
-from datetime import datetime
+	"""This is is the BaseModel of the project. which other class and instances will read from"""
 
-class BaseModel:
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != '__class__':
-                    setattr(self, key, value)
-            self.id = str(self.id)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+	def __init__(self, *args, **kwargs):
+		"""This helps to initialize BaseModel."""
 
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+		if kwargs:
+			for key, value in kwargs.items():
+				if key == 'created_at' or key == 'updated_at':
+					value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+				setattr(self, key, value)
+		else:
+			self.id = str(uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
+			models.storage.new(self)
+	def save(self):
+		"""
+		This helps to update the current time of the base model
+		datetime.now is used instead of datetime.today.
+		reason being that the now will give the current
+		date as of the date of execution.
+		"""
+		self.update_at = datetime.now()
+		models.storage.save()
+		
+	def to_dict(self):
+		"""
+		This returns the dictionary of Base Model in the project
+		"""
+		d = self.__dict__.copy()
+		d['__class__'] = self.__class__.__name__
+		d['created_at'] = self.created_at.isoformat()
+		d['updated_at'] = self.updated_at.isoformat()
+		return d
 
-    def save(self):
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        new_dict = {}
-        for key, value in self.__dict__.items():
-            if key == 'created_at' or key == 'updated_at':
-                value = value.isoformat()
-            new_dict[key] = value
-        new_dict['__class__'] = self.__class__.__name__
-        return new_dict
-
+	def __str__(self):
+		"""print representation of the BaseModel instance."""
+		return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.to_dict())
